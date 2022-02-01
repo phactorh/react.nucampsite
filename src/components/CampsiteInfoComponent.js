a62f78c9e1a0f720044a442c899646fbbb3212e5
Chris Hodge <chrishodge801@gmail.com>
2022-02-01 13:17:36 -0700

    Refactor into Presentation and Container

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -40,11 +40,14 @@ class CampsiteInfo extends Component {
     render() {if (this.props.campsite) {
             
         return(
-        <div className="row">
-            {this.renderCampsite(this.props.campsite)} 
-            {this.renderComments(this.props.campsite.comments)}
-        </div>);
-        
+            <div className="container">
+                <div className="row">
+                    
+                    {this.renderCampsite(this.props.campsite)} 
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            </div>
+        );
     }
         else {
          return(<div></div>);
