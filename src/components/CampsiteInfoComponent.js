8ec28c4dfcd3ced34b519c410772721c4462f4d0
Chris Hodge <chrishodge801@gmail.com>
2022-02-01 23:27:45 -0700

    Breadcrumb

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import {CAMPSITES} from '../shared/campsites'
 
     
@@ -36,15 +37,25 @@ function RenderCampsite({campsite}){
     
     function CampsiteInfo(props) {if (props.campsite) {
             
-        return(
+        return (
             <div className="container">
                 <div className="row">
-                    
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
+      
     }
         else {
          return(<div></div>);
